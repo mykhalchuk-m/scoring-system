253d51ca056c9139be6ba9f55e8dd2a8694bcba8
mykhalchuk.m@gmail.com <mykhalchuk.m@gmail.com@bb669e62-c18c-0840-c25e-0be1a007f76c>
2012-03-12 10:52:09 +0000

    adding raw parser

--- trunk/ScoringSystem/WebContent/js/data-parsing.js
+++ trunk/ScoringSystem/WebContent/js/data-parsing.js
@@ -1,12 +1,23 @@
 function parse() {
 	$.ajax({
 		type : "GET",
-		url : "data1.xml",
+		url : "data/data.xml",
 		dataType : "xml",
 		success : function(xml) {
-			alert(9);
-			alert(xml);
-			alert(1);
+			var content 	= "";
+			content += "<h3>" + $(xml).find("bean[id='bean2']").attr("name") + "</h3>";
+			i = 0;
+			$(xml).find("bean[id='bean1'] property").each(function() {
+				content += "<label>" + $(this).attr("name") + "</label>";
+				content += "<select name='prop" + i++ + "'>";
+				$(this).find("map entry").each(function() {
+					content += "<option value='" + $(this).attr('value') + "'>";
+					content += $(this).attr('key');
+					content += "</option>";
+				});
+				content += "</select><br/>";
+			});
+			$("#content").html(content);
 		}
 	});
 }
