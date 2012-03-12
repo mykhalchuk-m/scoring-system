398247131383239eb8cddebb86257ee588d89b12
mykhalchuk.m@gmail.com <mykhalchuk.m@gmail.com@bb669e62-c18c-0840-c25e-0be1a007f76c>
2012-03-12 11:19:58 +0000

    assign parser to view

--- ScoringSystem/WebContent/js/data-parsing.js
+++ ScoringSystem/WebContent/js/data-parsing.js
@@ -5,19 +5,20 @@ function parse() {
 		dataType : "xml",
 		success : function(xml) {
 			var content 	= "";
-			content += "<h3>" + $(xml).find("bean[id='bean2']").attr("name") + "</h3>";
+			content += "<fieldset><legend>" + $(xml).find("bean[id='bean2']").attr("name") + "</legend>";
 			i = 0;
 			$(xml).find("bean[id='bean1'] property").each(function() {
-				content += "<label>" + $(this).attr("name") + "</label>";
-				content += "<select name='prop" + i++ + "'>";
+				content += "<label for='fild" + i++ + "' class='col_4'>" + $(this).attr("name") + "</label>";
+				content += "<select class='fancy col_7' id='fild" + i + "' name='prop" + i + "'>";
 				$(this).find("map entry").each(function() {
 					content += "<option value='" + $(this).attr('value') + "'>";
 					content += $(this).attr('key');
 					content += "</option>";
 				});
-				content += "</select><br/>";
+				content += "</select>";
 			});
-			$("#content").html(content);
+			content += "</fieldset>";
+			$("#form-step").html(content);
 		}
 	});
 }
