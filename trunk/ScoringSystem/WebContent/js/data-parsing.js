5fec813b271a82684859c024e08b120af31f8b70
tararasik@gmail.com <tararasik@gmail.com@bb669e62-c18c-0840-c25e-0be1a007f76c>
2012-03-12 14:26:24 +0000

    Tabs added

--- trunk/ScoringSystem/WebContent/js/data-parsing.js
+++ trunk/ScoringSystem/WebContent/js/data-parsing.js
@@ -1,15 +1,20 @@
-function parse(k) {
+function parse(beanId) {
 	$.ajax({
 		type : "GET",
 		url : "data/data.xml",
 		dataType : "xml",
-		success : function(xml) {
-			var content 	= "";
-			content += "<fieldset><legend>" + $(xml).find("bean[id='bean" + k + "']").attr("name") + "</legend>";
-			i = 0;
-			$(xml).find("bean[id='bean" + k + "'] property").each(function() {
-				content += "<label for='fild" + i++ + "' class='col_4'>" + $(this).attr("name") + "</label>";
-				content += "<select class='fancy col_7' id='fild" + i + "' name='prop" + i + "'>";
+		success : function(xml) {		
+			var defaultValue = 42;
+			var defaultText = '-- Оберіть варіант --';
+			var content = "";
+			var i = 0;
+
+			content += "<form id='form-step" + beanId + "'><fieldset><legend>" + $(xml).find("bean[id='bean" + beanId + "']").attr("name") + "</legend>";
+
+			$(xml).find("bean[id='bean" + beanId + "'] property").each(function() {
+				content += "<label for='field" + ++i + "' class='col_4'>" + $(this).attr("name") + "</label>";
+				content += "<select class='fancy col_7' id='field" + i + "' name='prop" + i + "'>";
+				content += "<option value='" + defaultValue + "'>" + defaultText + "</option>";
 				$(this).find("map entry").each(function() {
 					content += "<option value='" + $(this).attr('value') + "'>";
 					content += $(this).attr('key');
@@ -18,8 +23,11 @@ function parse(k) {
 				content += "</select>";
 			});
 			content += "</fieldset>";
-			content += "<button id='submit' type='submit'>Далі<span class='icon' data-icon='}'/></button>";
-			$("#form-step").html(content);
+			content += "<button type='submit'>Далі<span class='icon' data-icon='}'/></button></form>";
+			
+			$("#tabr" + beanId).html(content);
 		}
 	});
 }
+
+
